25c96d2b1789ed118594d3f4b71fad26104a06c5
william hernandez <william@hernandez2.com>
2019-03-13 19:59:22 -0400

    typo

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ const Index = () => (
   <Layout>
           <div className="column is-marginBottom-25">
             <h3 className="is-biryany is-bold is-marginBottom-25">New website coming soon.</h3>
-            <h1 className="is-marginBottom  is-bold">WE BUILD DIGITAL PRODUCTS THAT EXPAND YOU BRAND AND BUSINESS.</h1>
+            <h1 className="is-marginBottom  is-bold">WE BUILD DIGITAL PRODUCTS THAT EXPAND YOUR BRAND AND BUSINESS.</h1>
             <br />
             <p className=" is-marginBottom is-size-5-mobile">
             Custom responsive websites and web applications.  Fixed ongoing prices you can budget for.
